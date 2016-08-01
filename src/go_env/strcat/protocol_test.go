a96ba7d23c3c7cb17e62d3325febb122c86b0271
Googol Lee <i@googol.im>
2016-08-01 11:17:08 +0800

    add quick test

--- src/go_env/strcat/protocol_test.go
+++ src/go_env/strcat/protocol_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"testing/quick"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -36,5 +37,15 @@ func TestHandler(t *testing.T) {
 			continue
 		}
 		at.Equal(w.Body.String(), t.output)
+	}
+}
+
+func TestJoinStrings(t *testing.T) {
+	f := func(s []string) bool {
+		out := joinStrings(s)
+		return strings.Join(s, "") == out
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
 	}
 }
